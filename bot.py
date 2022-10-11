9b6895838b4c0546642b8dc0d56fbc9345f9e0c7
Redhwan-Rahi <redhwanrahi1@gmail.com>
2022-10-10 20:52:57 -0400

    fixed modal and response

--- bot.py
+++ bot.py
@@ -12,7 +12,8 @@ from slackeventsapi import SlackEventAdapter
 import requests
 import utilities
 import json
-
+import time
+import threading
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
 
@@ -46,14 +47,17 @@ def message(payload):
 @app.route('/message-count', methods=['POST'])
 def message_count():
     data = request.form 
+    print(data)
     user_id = data.get('user_id')
     user_text = data.get('text')
-    channel_id = data.get('channel_id')   
-    message3 = utilities.tempMessage(user_id,user_text)
+    channel_id = data.get('channel_id')
+    messageHolder = utilities.tempMessage()
+    messageHolder['blocks'] = initiateR(user_text)
+    #message3 = utilities.tempMessage(user_id,user_text)
     headers = {'Authorization':f'Bearer {SLACK_BOT}', 'Content-type':'application/json'}
     response_url = data.get("response_url")
-    params = {'channel':f'{channel_id}', 'blocks':json.dumps(message3)}
-    requests.post(response_url, data=json.dumps(message3), headers=headers, verify=False)
+    params = {'channel':f'{channel_id}', 'blocks':json.dumps(messageHolder)}
+    requests.post(response_url, data=json.dumps(messageHolder), headers=headers, verify=False)
     return Response(), 200
 
 @app.route('/weather', methods=['GET','POST'])
@@ -72,37 +76,118 @@ def weather():
     # client.chat_postMessage(channel=channel_id, thread_ts=ts, text=f"Weather For: {text}. \nCurrent Conditions: {response['weather'][0]['description']}. \nCurrent Temperature: {temp:.2f}°F")
     return Response(data.to_dict), 200
     
+
+def getResponse():
+    tempArr = []
+    for i in range(25):
+        tempArr.append({'displayName':'US-AZ'+f'{i}', 'distinguishedName':'CN-AX-DC,=AE'+f'{i}'})
+    return tempArr
+
+def updateView(headers, params):
+    time.sleep(3)
+    requests.post("https://slack.com/api/views.update",headers=headers, data=params, verify=False).json()
+    return
+
 MOVIE_API_KEY = 'https://api.themoviedb.org/3/movie/upcoming?api_key=e4c0395b4795a399603e44610db36ddd'
 @app.route('/movie-dl', methods=['POST'])
 def movieList():
-    data = request.form 
-    user_text = data.get('text')
-    userInp = [x.strip() for x in user_text.split(',')]   
-    modal_data = utilities.payload() 
-    modal_data['blocks'][1]['element']['options'] = helper(userInp)
-    headers = {'Authorization':f'Bearer {SLACK_BOT}'}
-    params = {"trigger_id":data.get('trigger_id'), "view":json.dumps(modal_data)}
-    requests.post("https://slack.com/api/views.open",headers=headers, data=params, verify=False)
+    data = request.form
+    if(data.get('text')):
+        user_text = data.get('text')
+        if(user_text.count("@") == 1):
+            if(len(user_text) > 1 and '@' in user_text):
+                ads_id = (user_text.split('|')[1].strip('<>'))
+                unique_id = data.get('text').split('|')[0].split('@')[1]
+                print(ads_id)
+            headers = {'Authorization':f'Bearer {SLACK_BOT}'}
+            params = {'user':unique_id}
+            modal_data = utilities.payload()
+            temp_data = utilities.loading()
+            params = {"trigger_id":data.get('trigger_id'), "view":json.dumps(temp_data)}
+            modalPostRequest = requests.post("https://slack.com/api/views.open",headers=headers, data=params, verify=False)
+            responseData = getResponse()
+            response_url = data.get('response_url')
+            modal_data['blocks'][0]['element']['options'] = utilities.buildList(responseData)
+            modalViewId = (modalPostRequest.json().get('view')['id'])
+            params = {"view_id":modalViewId, "view":json.dumps(modal_data)}
+            t = threading.Thread(target=updateView, args=(headers, params))
+            t.start()
+            return '', 200
+        else:
+            return jsonify(response='error t2o'), 200
+    else:
+        return jsonify(response='error'), 200
+    # data = request.form 
+    # user_text = data.get('text')
+    # print(user_text.split('|')[1].strip('<>'))
+    # user_inp = user_text.split(' ')
     
-    return Response(), 200
+    # headers = {'Authorization':f'Bearer {SLACK_BOT}'}
+    # response_url = data.get('response_url')
+    # if(len(user_inp) > 1):
+    #     msg = utilities.errorMessage()
+    #     msg['blocks'] = [{
+    #             "type": "section",
+    #             "text": {
+    #                 "type": "plain_text",
+    #                 "text": "Please try again and @ only 1 hiring leader."
+    #             }
+    #         }]
+    #     requests.post(response_url, data=json.dumps(msg), headers=headers, verify=False)
+    #     return Response(), 200
+    # elif(len(user_inp) == 1 and user_inp[0] == ''):
+    #     msg = utilities.errorMessage()
+    #     msg['blocks'] = [{
+    #             "type": "section",
+    #             "text": {
+    #                 "type": "plain_text",
+    #                 "text": "Please try again and @ a hiring leader."
+    #             }
+    #         }]
+    #     requests.post(response_url, data=json.dumps(msg), headers=headers, verify=False)
+    #     return Response(), 200
+    # else:
+    #     modal_data = utilities.payload() 
+    #     modal_data['blocks'][0]['element']['options'] = helper()
+    #     params = {"trigger_id":data.get('trigger_id'), "view":json.dumps(modal_data)}
+    #     requests.post("https://slack.com/api/views.open",headers=headers, data=params, verify=False)
+        
+    # return Response(), 200
 
-def helper(userInp):
+INPU = []
+for i in range(80):
+    INPU.append({'displayName':'US-AZ'+f'{i}', 'distinguishedName':'CN=-AEXO,DC.com'+f'{i}'})
+
+def helper():
     # response = requests.get(MOVIE_API_KEY).json()
     titles = []
-    for inp in userInp:
-        MOVIE_API_KEY2 = 'https://api.themoviedb.org/3/movie/{}?api_key=e4c0395b4795a399603e44610db36ddd'.format(inp)
-        try:
-            response = requests.get(MOVIE_API_KEY2).json()
-            titles.append({
+    res = []
+    print(INPU)
+    for idx,x in enumerate(INPU):
+        if(idx > 99):
+            break
+        res.append({
             "text":{
                 "type":"plain_text",
-                "text":"{}".format(response['title'])
+                "text":"{}".format(x['displayName'])
             },
-            "value":"{}".format("idx")
+            "value":"{}".format(x['distinguishedName'])
         })
-        except KeyError:
-            continue
-    return titles
+    return res
+    # for inp in userInp:
+    #     MOVIE_API_KEY2 = 'https://api.themoviedb.org/3/movie/{}?api_key=e4c0395b4795a399603e44610db36ddd'.format(inp)
+    #     try:
+    #         response = requests.get(MOVIE_API_KEY2).json()
+    #         titles.append({
+    #         "text":{
+    #             "type":"plain_text",
+    #             "text":"{}".format(response['title'])
+    #         },
+    #         "value":"{}".format("idx")
+    #     })
+    #     except KeyError:
+    #         continue
+    # return titles
     # titles = []
     # for idx, x in enumerate(response.get('results')):
     #     titles.append({
@@ -118,10 +203,20 @@ def helper(userInp):
 @app.route('/interaction', methods=['POST','GET','PUT'])
 def receive():
     data = request.form.to_dict()
-    print(data)
     data_payload = json.loads(data['payload'])
-    if data_payload['view']['callback_id'] == 'distribution-list':
-        print("HELLO WORLD")   
+    if data_payload['view']['callback_id'] == 'DL':
+        obj = data_payload['view']['state']['values']
+        # print(obj)
+        listInput = (obj.get('list_selector').get('multi_static_select-action').get('selected_options'))
+        textInput = (obj.get('name_input').get('plain_text_input-action').get('value'))
+        for x in listInput:
+            print(x.get('value'))
+        arr = []
+        for x in textInput.split(','):
+            print(x)
+            if(x.strip(' ').isalnum()):
+                arr.append(x.strip(' '))
+        print(arr)   
     return Response(), 201
 
 @app.route('/preboard-dl', methods=['POST','GET','PUT'])
@@ -161,6 +256,30 @@ def preboard():
     requests.post(response_url, data=json.dumps(message3), headers=headers, verify=False)
 
     return Response(), 200
+
+def initiateR(user_text):
+    arr = []
+    headers = {'Authorization':f'Bearer {SLACK_BOT}', 'Content-type':'application/json'}
+    for x in (user_text.split(' ')):
+        try:
+            if '|' and '@' in x:
+                temp = (x.split('|')[1].strip('<>'))
+                iD = (x.split('|')[0].strip('@<>'))
+                params = {"user":iD}
+                response = requests.get('https://slack.com/api/users.info',headers=headers, data=params)
+                print(response)
+                print(iD, "HI")
+                arr.append({
+                "type":"section",
+                "text":{
+                    "type":"mrkdwn",
+                    "text":"Hi {} :wave:".format(temp)
+                }
+                }
+            )
+        except:
+            continue
+    return arr
 
 if __name__ == "__main__":
     app.run(debug=True)
--- utilities.py
+++ utilities.py
@@ -1,3 +1,5 @@
+
+
 invalid_users = {
     "blocks": [
         {
@@ -10,26 +12,33 @@ invalid_users = {
     ]
 }
 
-def tempMessage(user_id, user_text):
+def tempMessage():
     message = {
         "blocks":[
             {
                 "type":"section",
                 "text":{
                     "type":"mrkdwn",
-                    "text":"Hi {} dsadasdsadasdasdasd:wave:".format(user_id)
+                    "text":"Hi  dsadasdsadasdasdasd:wave:"
                 }
             },
+        ]
+    }
+    return message
+
+def errorMessage():
+    msg = {
+        "blocks": [
             {
-                "type":"section",
-                "text":{
-                    "type":"mrkdwn",
-                    "text":"Everything you typed: {}".format(user_text)
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Please @ the hiring leader"
                 }
             }
         ]
     }
-    return message
+    return msg
 
 def tempMessage1(user_id):
     message1 = [
@@ -43,10 +52,99 @@ def tempMessage1(user_id):
     ]
     return message1
 
+# def payload():
+#     res = {
+# 	"type": "modal",
+# 	"title": {
+# 		"type": "plain_text",
+# 		"text": "My App"
+# 	},
+# 	"submit": {
+# 		"type": "plain_text",
+# 		"text": "Submit"
+# 	},
+# 	"close": {
+# 		"type": "plain_text",
+# 		"text": "Cancel"
+# 	},
+# 	"blocks": [
+# 		{
+# 			"type": "input",
+# 			"element": {
+# 				"type": "plain_text_input",
+# 				"action_id": "plain_text_input-action"
+# 			},
+# 			"label": {
+# 				"type": "plain_text",
+# 				"text": "Label"
+# 			}
+# 		},
+# 		{
+# 			"type": "section",
+# 			"text": {
+# 				"type": "mrkdwn",
+# 				"text": "Test block with multi static select"
+# 			},
+# 			"accessory": {
+# 				"type": "multi_static_select",
+# 				"placeholder": {
+# 					"type": "plain_text",
+# 					"text": "Select options"
+# 				},
+# 				"options": [
+# 					{
+# 						"text": {
+# 							"type": "plain_text",
+# 							"text": "*this is plain_text text*"
+# 						},
+# 						"value": "value-0"
+# 					}
+# 				],
+# 				"action_id": "multi_static_select-action",
+# 				"max_selected_items": 2,
+                
+# 			}
+# 		}
+# 	]
+# }
+#     return res
+def loading():
+    msg = {
+        "type": "modal",
+        "title": {
+            "type": "plain_text",
+            "text": "My App"
+        },
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": ":man-biking: Now loading..."
+                }
+            }
+        ]
+    }
+    return msg
+
+def buildList(input_arr):
+    distributionLists = []
+    for idx, elem in enumerate(input_arr):
+        if(idx > 99):
+            break
+        distributionLists.append({
+            "text":{
+                "type":"plain_text",
+                "text":"{}".format(elem['displayName'])
+            },
+            "value":"{}".format(elem['distinguishedName'])
+        })
+    return distributionLists
+
 def payload():
     res = {
         "type": "modal",
-        "callback_id":"distribution-list",
+        "callback_id":"DL",
         "title": {
             "type": "plain_text",
             "text": "My App"
@@ -62,9 +160,23 @@ def payload():
         "blocks": [
             {
                 "type": "input",
+                "block_id":"list_selector",
                 "element": {
-                    "type": "plain_text_input",
-                    "action_id": "plain_text_input-action"
+                    "type": "multi_static_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select options"
+                    },
+                    "options": [
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": ":man-biking: Now loading..."
+                            },
+                            "value": "value-0"
+                        },
+                    ],
+                    "action_id": "multi_static_select-action"
                 },
                 "label": {
                     "type": "plain_text",
@@ -73,22 +185,10 @@ def payload():
             },
             {
                 "type": "input",
+                "block_id":"name_input",
                 "element": {
-                    "type": "static_select",
-                    "placeholder": {
-                        "type": "plain_text",
-                        "text": "Select an item"
-                    },
-                    "options": [
-                        {
-                            "text": {
-                                "type": "plain_text",
-                                "text": "*this is plain_text text*"
-                            },
-                            "value": "value-0"
-                        }
-                    ],
-                    "action_id": "static_select-action"
+                    "type": "plain_text_input",
+                    "action_id": "plain_text_input-action"
                 },
                 "label": {
                     "type": "plain_text",
@@ -96,5 +196,62 @@ def payload():
                 }
             }
         ]
-    }
+}
     return res
+
+
+# def payload():
+#     res = {
+#         "type": "modal",
+#         "callback_id":"distribution-list",
+#         "title": {
+#             "type": "plain_text",
+#             "text": "My App"
+#         },
+#         "submit": {
+#             "type": "plain_text",
+#             "text": "Submit"
+#         },
+#         "close": {
+#             "type": "plain_text",
+#             "text": "Cancel"
+#         },
+#         "blocks": [
+#             {
+#                 "type": "input",
+#                 "element": {
+#                     "type": "plain_text_input",
+#                     "action_id": "plain_text_input-action"
+#                 },
+#                 "label": {
+#                     "type": "plain_text",
+#                     "text": "Label"
+#                 }
+#             },
+#             {
+#                 "type": "input",
+#                 "element": {
+#                     "type": "static_select",
+#                     "placeholder": {
+#                         "type": "plain_text",
+#                         "text": "Select an item"
+#                     },
+#                     "options": [
+#                         {
+#                             "text": {
+#                                 "type": "plain_text",
+#                                 "text": "*this is plain_text text*"
+#                             },
+#                             "value": "value-0"
+#                         }
+#                     ],
+#                     "action_id": "static_select-action"
+#                 },
+#                 "label": {
+#                     "type": "plain_text",
+#                     "text": "Label"
+#                 }
+#             }
+#         ]
+#     }
+#     return res
